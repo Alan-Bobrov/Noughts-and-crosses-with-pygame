d8644714fd38b81cb6f947e5a9269769f550985e
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-11-13 14:49:19 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,2 +1,9 @@
 def СhangeСoordinates(x, y) -> tuple:
-    return 1, 1
+    new_x, new_y = None, None
+    for i in range(3):
+        if 280 + 112 * i <= x:
+            new_x = i
+        if 160 + 112 * i <= y:
+            new_y = i
+    
+    return new_x, new_y
