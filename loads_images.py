fdbc288dc2109064fbc972874dff59e6e31dd071
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-11-13 13:38:52 +0300

    Update loads_images.py

--- loads_images.py
+++ loads_images.py
@@ -1,1 +1,18 @@
+from pygame.image import load
+
+def Load(image):
+    return load(f"images/{image}.png")
+
+FieldImg = Load("field")
+
+Zero = Load("zero")
+One = Load("one")
+Two = Load("two")
+Three = Load("three")
+Four = Load("four")
+Five = Load("five")
+Six = Load("six")
+Seven = Load("seven")
+Eight = Load("eight")
+Nine = Load("nine")
 
