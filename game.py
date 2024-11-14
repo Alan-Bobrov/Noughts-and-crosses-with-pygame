131503bd6ee24d1fad48b284ce84ebbb876a7418
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-11-14 19:05:13 +0300

    Update game.py

--- game.py
+++ game.py
@@ -11,30 +11,65 @@ from classes import *
 from loads_images import *
 from functions import *
 
+
 def NoughtsAndCrosses():
     pg.init()
     screen = pg.display.set_mode((600, 600))
     screen.fill((255, 255, 255))
     screen.blit(FieldImg, (0, 0))
 
+    field = Field()
     is_game = True
     infinite_mode = False
     now_move = "X" # X or O
     tournament_mode = False
     X_score = 0
-    Y_score = 0
+    O_score = 0
     winner_found = False
+    digits = {
+        "0": Zero,
+        "1": One,
+        "2": Two,
+        "3": Three,
+        "4": Four,
+        "5": Five,
+        "6": Six,
+        "7": Seven,
+        "8": Eight,
+        "9": Nine
+    }
 
-    field = Field()
+    def ClearField(score, who, X_score, O_score, screen):
+        if score:
+            if who == "X":
+                X_score += 1
+            elif who == "O":
+                O_score += 1
 
-    # #screen.blit(Cross, (280, 160))
-    # screen.blit(Cross, (280, 160))
-    # # screen.blit(Cross, (280 + 112, 160))
-    # # screen.blit(Cross, (280 + 112 * 2, 160))
-    # screen.blit(Cross, (280, 160 + 112))
+        field = Field()
+        now_move = "X"
+        winner_found = False
+
+        screen = pg.display.set_mode((600, 600))
+        screen.fill((255, 255, 255))
+        screen.blit(FieldImg, (0, 0))
+
+        return X_score, O_score, field, now_move, winner_found
+
+
     while is_game:
         screen.blit(FieldImg, (0, 0))
         field.DrawField(screen)
+        if tournament_mode == True:
+            screen.blit(TournamentMenu, (0, 152))
+            O_score_str = str(O_score)
+            X_score_str = str(X_score)
+
+            for i in range(len(X_score_str)):
+                screen.blit(digits[X_score_str[i]], (120 + i * 48, 280))
+
+            for i in range(len(O_score_str)):
+                screen.blit(digits[O_score_str[i]], (120 + i * 48, 392))
         if now_move == "X":
             screen.blit(Cross, (496,  48))
         else:
@@ -58,12 +93,19 @@ def NoughtsAndCrosses():
                     if end_attack[1] == True:
                         if tournament_mode == True:
                             if now_move == "X":
-                                X_score += 1
+                                X_score, O_score, field, now_move, winner_found = ClearField(True, "X", X_score, O_score, screen)
                             else:
-                                Y_score += 1
+                                X_score, O_score, field, now_move, winner_found = ClearField(True, "O", X_score, O_score, screen)
                         else:
-                            print(f"Win code will be here (winner - {now_move}) ")
+                            if now_move == "X":
+                                screen.blit(CrossWon, (40, 224))
+                            elif now_move == "O":
+                                screen.blit(NoughtWon, (40, 224))
                             winner_found = True
+
+                    if winner_found == False and field.CountFreePlace() == 0:
+                        winner_found = True
+                        screen.blit(Draw, (16, 224))
                     
                     if end_attack[0]:
                         if now_move == "X":
@@ -71,21 +113,22 @@ def NoughtsAndCrosses():
                         else:
                             now_move = "X"
 
-                elif (337 <= x <= 536) and (488 <= y <= 575):
-                    screen.blit(FieldImg, (0, 0))
-                    
-                    field = Field()
-                    now_move = "X"
-                    winner_found = False
-
-                    screen = pg.display.set_mode((600, 600))
-                    screen.fill((255, 255, 255))
-                    screen.blit(FieldImg, (0, 0))
+                elif ((336 <= x <= 536) and (488 <= y <= 576)) or ((328 <= x <= 544) and (496 <= y <= 568)) or ((320 <= x <= 552) and (504 <= y <= 560)):
+                    X_score, O_score, field, now_move, winner_found = ClearField(False, None, X_score, O_score, screen)
             
                 elif (x - 63)**2 + (y - 63)**2 <= 60**2 :
                     print("--------", x, y)
                 
+                elif tournament_mode == True:
+                    if ((32 <= x <= 232) and (488 <= y <= 576)) or ((24 <= x <= 240) and (496 <= y <= 568)) or ((16 <= x <= 248) and (504 <= y <= 560)):
 
+                        X_score = 0
+                        O_score = 0
+                        screen = pg.display.set_mode((600, 600))
+                        screen.fill((255, 255, 255))
+                        screen.blit(FieldImg, (0, 0))
+
+                        print("---------------2")
 
 
         pg.display.flip()
