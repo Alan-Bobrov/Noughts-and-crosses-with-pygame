09802fee29cec3bdc49fe2727f1ebfb8b83a9487
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-12-09 21:01:59 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+from classes import *
+
 def СhangeСoordinates(x, y) -> tuple:
     new_x, new_y = None, None
     for i in range(3):
@@ -7,3 +9,20 @@ def СhangeСoordinates(x, y) -> tuple:
             new_y = i
     
     return new_x, new_y
+
+def ClearField(score, who, X_score, O_score, screen):
+        if score:
+            if who == "X":
+                X_score += 1
+            elif who == "O":
+                O_score += 1
+
+        field = Field()
+        now_move = "X"
+        winner_found = False
+
+        screen = pg.display.set_mode((600, 600))
+        screen.fill((255, 255, 255))
+        screen.blit(FieldImg, (0, 0))
+
+        return X_score, O_score, field, now_move, winner_found
